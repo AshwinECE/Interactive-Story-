829211f940f169592d18d3876b75414af235260e
ashwiniyer <ashwinscool@gmail.com>
2017-04-30 18:17:03 -0400

    Basic Story transitions working. Next steps will be to refactor and add final media touches as well as user input for the story

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,13 @@ class ViewController: UIViewController {
     }
 
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "startAdventure"){
+            guard let pageController = segue.destination as? PageController else {
+                return
+            }
+            pageController.page = Adventure.story
+        }
+    }
 }
 
